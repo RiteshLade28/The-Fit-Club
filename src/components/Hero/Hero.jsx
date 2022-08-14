91efdea6c33854c0a0c38f29520c1c34fb98552a
RiteshLade28 <riteshlade10@gmail.com>
2022-08-14 21:05:28 +0530

    Completed join us component, blurs and email section using emailjs

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,10 +5,11 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
-
+ 
 export const Hero = () => {
     return (
         <div className='hero'>
+            <div className='blur hero-blur'></div>
             <div className='left-h'>
                 <Header />
                 <div className='the-best-ad'>
--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,44 +1,43 @@
 import React, { useRef } from 'react';
 import './Join.css';
-// import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 
 const Join = () => {
-
-
-    const form = useRef();
+    const form = useRef()
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_vnsexma', 'template_s6vjwi9', form.current, 'YOUR_PUBLIC_KEY')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
+        emailjs.sendForm('service_vnsexma', 'template_s6vjwi9', form.current, 'MUQ6ZQS-_l1gH_O7y')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
 
-    return (
-        <div className="Join" id="join-us">
-            <div className="left-j">
-                <hr />
-                <div>
-                    <span className="stroke-text">READY TO</span>
-                    <span>LEVEL UP</span>
-                </div>
-                <div>
-                    <span>YOUR BODY</span>
-                    <span className="stroke-text">WITH US?</span>
-                </div>
+
+  return (
+    <div className="Join" id="join-us">
+        <div className="left-j">
+            <hr />
+            <div>
+                <span className="stroke-text">READY TO</span>
+                <span>LEVEL UP</span>
             </div>
-            <div className="right-j">
-                <form ref={form} className="email-container" onSubmit={sendEmail}>
-                    <input type="email" name="user_email" placeholder="Enter your email address" />
-                    <button className="btn btn-j">Join Now</button>
-                </form>
+            <div>
+                <span>YOUR BODY</span>
+                <span className="stroke-text">WITH US?</span>
             </div>
         </div>
-    )
+        <div className="right-j">
+            <form ref={form} className="email-container" onSubmit={sendEmail}>
+                <input type="email" name="user_email" placeholder="Enter your email address" />
+                <button className="btn btn-j">Join Now</button>
+            </form>
+        </div>
+    </div>  
+  )
 }
 
 export default Join
