69ebe91a842cd87e6b84430842ca29c8fe2020c4
RiteshLade28 <riteshlade10@gmail.com>
2022-08-15 23:50:10 +0530

    Fully ready website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion/dist/framer-motion'
 import NumberCounter from "number-counter";
 
 export const Hero = () => {
